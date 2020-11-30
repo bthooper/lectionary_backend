3a50a26bc5012869ebed0004b8b262f4c8316213
Bryan Hooper <bthooper@gmail.com>
2020-11-30 11:22:15 -0500

    added model and migration for Days

---
+++ db/migrate/20201130162156_create_days.rb
@@ -0,0 +1,13 @@
+class CreateDays < ActiveRecord::Migration[6.0]
+  def change
+    create_table :days do |t|
+      t.string :name
+      t.string :description
+      t.string :startDate
+      t.string :endDate
+      t.references :Season, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
