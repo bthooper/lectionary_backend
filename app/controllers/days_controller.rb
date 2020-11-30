5566d9ecb4b5fad3aba7827a0ede187280a15e35
Bryan Hooper <bthooper@gmail.com>
2020-11-30 14:19:28 -0500

    began work on routes/controllers for api

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,14 @@
+class DaysController < ApplicationController
+
+  def index
+    days = Day.all
+    render json: days
+  end
+
+  def show
+    day = Day.find(params[:id])
+    render json: day
+  end
+  
+end
+
